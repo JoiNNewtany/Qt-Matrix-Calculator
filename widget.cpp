f956bed7e6483a050bf057089ac1abb335361fbf
Solar <join200022@gmail.com>
2019-12-26 02:28:45 +0300

    Added matrix resizing and value checking function.

--- widget.cpp
+++ widget.cpp
@@ -1,6 +1,15 @@
 #include "widget.h"
 #include "ui_widget.h"
 
+#include "QFileDialog"
+#include "QVector"
+#include "QFile"
+#include "QTextStream"
+#include "QDebug"
+#include "QTableWidget"
+#include "QTableWidgetItem"
+#include "QRegularExpression"
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
@@ -13,3 +22,48 @@ Widget::~Widget()
     delete ui;
 }
 
+void Widget::fillMatrixTable(QTableWidget *matrixTable, int rows, int columns)
+{
+    // Set rows and columns for the table
+
+    matrixTable->setRowCount(rows);
+    matrixTable->setColumnCount(columns);
+
+    // Fill them with empty items
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrixTable->setItem(i, j, new QTableWidgetItem);
+        }
+    }
+}
+
+bool Widget::isValueCorrect(QString value)
+{
+    // Check if a value is a number
+
+    QRegularExpression regex("^-?[0-9]\\d*(\\.\\d+)?$");
+    return regex.match(value).hasMatch();
+}
+
+
+// Slots //
+
+
+void Widget::on_sizeBox_valueChanged(int arg1)
+{
+    // Change size of all matrices to arg1
+
+    fillMatrixTable(ui->matrixTable1, arg1, arg1);
+    fillMatrixTable(ui->matrixTable2, arg1, arg1);
+    fillMatrixTable(ui->matrixTable3, arg1, arg1);
+}
+
+void Widget::on_matrixTable1_cellChanged(int row, int column)
+{
+    /* If a cell is changed, check if an index in
+       the selectionBox is selected and if new data is correct.
+       If so, overwrite that matrix in the global list of matrices. */
+}
--- widget.h
+++ widget.h
@@ -2,6 +2,7 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QTableWidget>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -15,7 +16,22 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
+    // Changes size of a matrix table and Fills it with empty elements
+    void fillMatrixTable(QTableWidget *matrixTable, int rows, int columns);
+
+    // Checks if a given value is correct for writing to table
+    bool isValueCorrect(QString value);
+
+private slots:
+    void on_sizeBox_valueChanged(int arg1);
+
+    void on_matrixTable1_cellChanged(int row, int column);
+
 private:
     Ui::Widget *ui;
+    // QVector<QSquareMatrix> matrixList, tableList;
+    // MatrixList is the global vector of imported matrices.
+    // TableList will consist of three matrices from matrix tables
+    // where 0 is first matrix, 1 is second and 2 is result matrix.
 };
 #endif // WIDGET_H
